9b007e3a99faa6594b77776f7d198f41c4d86378
Ralitsa Narayan <ralitsanarayan@gmail.com>
2024-06-20 18:08:57 +0530

    document error in js

--- script.js
+++ script.js
@@ -3,86 +3,89 @@ let popupRef = document.querySelector(".popup");
 let newgameBtn = document.getElementById("new-game");
 let restartBtn = document.getElementById("restart");
 let msgRef = document.getElementById("message");
- //winning pattern array
- let winningPattern = [ [0,1,2], [0,3,6], [2,5,8], [6,7,8], [3,4,5],[1,4,7], [0,4,8],[2,4,6],
-];
-//player 'X' plays first
+
+//Winning Pattern Array
+let winningPattern = [ [0,1,2], [0,3,6], [2,5,8], [6,7,8], [3,4,5], [1,4,7], [0,4,8], [2,4,6]];
+
+//Player 'X' plays First
 let xTurn = true;
 let count = 0;
 
-//dsiable all buttons
-const disableButtons = () => {
-    btnRef.forEach((element) => (element.disabled = true));
-    //enable popup
+const disableButtons = () =>{
+    btnRef.forEach((element) => (element.disabled=true));
     popupRef.classList.remove("hide");
 };
 
-//enable all buttons(for new game and restart)
 const enableButtons = () => {
     btnRef.forEach((element) => {
         element.innerText = "";
-        element.disabled = "false";
+        element.disabled = false;
     });
-    //dsiable popup
     popupRef.classList.add("hide");
 };
 
-//new game
+const winFunction = (letter) => {
+    disableButtons();
+    if(letter == "X"){
+        msgRef.innerHTML = "&#x1F389; <br> 'X' Wins";
+    }
+    else{
+        msgRef.innerHTML = "&#x1F389; <br> 'O' Wins";
+    }
+};
+
 newgameBtn.addEventListener("click", () => {
+    count = 0 ;
+    enableButtons();
+});
+
+restartBtn.addEventListener("click", () => {
     count = 0;
     enableButtons();
 });
 
-//this function is executed when a player wins
-function winFunction(letter) {
+const drawFunction = () => {
     disableButtons();
+    msgRef.innerHTML = "&#x1F60E; <br> it's a draw";
+    count = 0; // reset count
+    enableButtons(); // re-enable buttons
+    msgRef.innerHTML = ""; // clear message
 }
-
-//win logic
-const winChecker = () => {
-    //loop through all win patterns
-    for (let i of winningPattern){
-        let[element1,element2, element3] = [
-            btnRef[i[0]].innerText,
-            btnRef[i[1]].innerText,
+//win Logic
+const winChecker = () =>{
+    for(let i of winningPattern){
+        let[element1, element2, element3] = [
+            btnRef[i[0]].innerText, 
+            btnRef[i[1]].innerText, 
             btnRef[i[2]].innerText,
         ];
-        //check if elements are filled
-        //3 elements are sae and would give win as would
-        if(element1 != "" && element2 != "" & element3 != ""){
+        //Chcek if elements are filled
+        if(element1 != "" && (element2 != "") & (element3 != "")){
             if(element1 == element2 && element2 == element3){
-                //if all 3 buttons have same values then pass the values to winFunction
                 winFunction(element1);
             }
         }
-    }
-};
+    };
+}
 
-//display X/O on click
 btnRef.forEach((element) => {
-    element.addEventListener("click", () => {
-    if (xTurn){
-        xTurn = false;
-        //display x
-        element.innerText = "X";
-        element.disabled = true;
-    }
-    else{
-        xTurn = true;
-        //display o
-        element.innerText = "O";
-        element.disabled = true;
-    }
-    //increment count
-    count +=1;
-    if(count === 9){
-        //its a draw
-
-    }
-    //check for win on every click
-    winChecker();
-  });
+    element.addEventListener("click", () =>{
+        if(xTurn){
+            xTurn = false;
+            element.innerText = "X";
+            element.disabled = true;
+         }
+         else{
+            xTurn = true;
+            element.innerText = "O";
+            element.disabled = true;
+         }
+         count +=1;
+         if(count === 9){
+            drawFunction();
+         }
+         winChecker();
+    });
 });
 
-//enable buttons and disable popup on page load
 window.onload = enableButtons;
